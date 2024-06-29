6ce86a750e3341575deb87e01f9386e97b46bd53
arpita <arpitaagrahari004@gmail.com>
2024-06-29 10:40:44 +0530

    Update homepage image slider functionality

--- Biblio_Frontend/js/homepage.js
+++ Biblio_Frontend/js/homepage.js
@@ -12,9 +12,3 @@ function nextSlide() {
     currentIndex = (currentIndex + 1) % totalSlides;
     showSlide(currentIndex);
 }
-
-setInterval(nextSlide, 3000);
-
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentIndex);
-});
