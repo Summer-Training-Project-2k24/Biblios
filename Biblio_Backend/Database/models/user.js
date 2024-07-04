e92993d759033b6db641dcdd69259efad729280a
Shavi Sharma <mail4shavi@gmail.com>
2024-07-04 22:25:01 +0530

    user

--- Biblio_Backend/Database/models/user.js
+++ Biblio_Backend/Database/models/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true },
 });
 
 const userModel = mongoose.model('User', userSchema);
