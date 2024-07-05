0f41cac9f29e5b55ad303a84f2d24e1082c4eac4
arpita <arpitaagrahari004@gmail.com>
2024-07-05 22:44:46 +0530

    Implementation of ai integration using api

--- ai.js
+++ ai.js
@@ -3,11 +3,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyABh80gEJruqmVwGEDUktELCIzwQvTpjdg");
 
-async function run(name, problem) {
+async function recommandation(ask,reply) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  const prompt = `You are lord krishna and i want you to write letter to me my name is ${name} and my problem in life is ${problem} and suggest me refreneces from bhagwat geeta as blessings to motivate and be as guide`
+  const prompt = `You are booklover and i want you to recommand book accoring to the asked ${ask} with little bit of details and preface of the book in ${reply}`
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -15,4 +15,13 @@ async function run(name, problem) {
   console.log(text);
 }
 
-run("Arpita","Suggest me be strong...");
+recommandation("Suggest me book aacording the suspense ","Reaper...");
+
+
+// app.post('/'(req,res)=>{
+//     const name = req.body.name;
+//     const problem = req.body.problem;
+
+//     const result = run(name,problem);
+//     res.json({letter: result});
+// })
