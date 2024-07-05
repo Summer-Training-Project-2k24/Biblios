f2a2457cd8bacdc53a092f193b0fa9e18812c01d
arpita <arpitaagrahari004@gmail.com>
2024-07-05 22:21:48 +0530

    Updated integration

--- ai.js
+++ ai.js
@@ -3,11 +3,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyABh80gEJruqmVwGEDUktELCIzwQvTpjdg");
 
-async function run() {
+async function run(name, problem) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = `You are lord krishna and i want you to write letter to me my name is ${name} and my problem in life is ${problem} and suggest me refreneces from bhagwat geeta as blessings to motivate and be as guide`
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -15,4 +15,4 @@ async function run() {
   console.log(text);
 }
 
-run();
+run("Arpita","Suggest me be strong...");
