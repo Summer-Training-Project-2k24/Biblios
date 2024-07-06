46c180387996b49afc58cfabcbb0c139aa1839a4
arpita <arpitaagrahari004@gmail.com>
2024-07-06 15:21:30 +0530

    refactor: Update ImageSlider component CSS for improved layout and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import './App.css';
+import ImageSlider from './components/ImageSlider';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Categories from './components/Categories';
@@ -11,6 +12,7 @@ function App() {
     return (
       <div className="App">
         <Header />
+        <ImageSlider />
         <Hero />
         <Categories />
         <NewRelease />
