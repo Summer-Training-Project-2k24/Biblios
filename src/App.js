428dda2103db90df733d8c2e5d8d97de9d43185e
arpita <arpitaagrahari004@gmail.com>
2024-07-06 15:04:11 +0530

    Refactor App.js to include Header, Hero, Categories, and NewRelease components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Categories from './components/Categories';
+import NewRelease from './components/NewRelease';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Hero />
+                <Categories />
+                <NewRelease />
+            </main>
+        </div>
+    );
 }
 
 export default App;
