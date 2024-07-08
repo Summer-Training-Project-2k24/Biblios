6633e5c3ce54a3a5afc6d998228f571fd0521949
Shavi Sharma <mail4shavi@gmail.com>
2024-07-08 23:41:11 +0530

    coonectionFunction

--- Backend/apiserver.js
+++ Backend/apiserver.js
@@ -1,2 +1,16 @@
 //conection to database
 
+const mongoose=require('mongoose');
+const connectDB=async()=>{
+    try{
+        const conn=await mongoose.connect(
+            'mongodb+srv://mail4shavi:6k3rFOJBW82jtwIx@cluster0.xzamzuy.mongodb.net/BookStote?retryWrites=true&w=majority&appName=Cluster0',
+           );
+            console.log('MongoDB Connected');
+    }catch(error){
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+module.exports=connectDB;
