a7f9e912dfe076e1e18a2aff9e58480458d93eff
Shavi Sharma <mail4shavi@gmail.com>
2024-07-10 23:59:22 +0530

    imports and constants

--- Backend/apiserver.js
+++ Backend/apiserver.js
@@ -1,4 +1,5 @@
 //Function to connect to database
+//you have to add this code in db.js 
 
 const mongoose=require('mongoose');
 
@@ -12,3 +13,25 @@ function connectwithDB(){
 
 module.exports=connectwithDB;
 
+//your server or index.js file should contain these imports and constants for the following code to run
+//imports and constants and other necessatities
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectwithDB = require('./connect.js');
+const asyncHandler = require('express-async-handler');
+require('dotenv').config();
+
+const ContactMessage = require('./models/contactus.js')
+
+const bookModel = require('./models/books.js');
+const books = require("./sample.json");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+connectwithDB();
