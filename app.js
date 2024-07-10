36eb209d8f0bbaf6e0b4b7716d9f9b9b007e0f14
Medhansh <medhansh0907@gmail.com>
2024-07-10 12:46:11 +0530

    commit changes

---
+++ app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = process.env.PORT || 5430;
+mongoose.connect('mongodb://localhost:27017/blogify').then(() => console.log("Mongodb connected"))
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+
+ 
