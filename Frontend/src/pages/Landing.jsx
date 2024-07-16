8b8556bd8b14db75648a6fc43562b796b2413f46
Shavi Sharma <mail4shavi@gmail.com>
2024-07-17 02:10:11 +0530

    landing page

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,11 +1,24 @@
-import Navbar from "../components/Navbar"
-import Slider from "../components/Slider"
-import Categories from "../components/Categories"
+import Navbar from "./navbar"
+import Slider from "./slider"
+//import Categories from "./categories"
+import WhyUsSlider from './WhyUsSlider'; 
 
-export default function () {
+import AuthorSlider from './AuthorSlider';
+import Footer from "./footer";
+import BookGenresSection from "./genre";
+
+const Landing=()=> {
     return <div>
         <Navbar />
         <Slider />
-        <Categories one="Higher Education" two="Management Books" three="sldjhfuiahfui" />
+        {/* <Categories one="Higher Education" two="Management Books" three="" /> */}
+        <AuthorSlider />
+        <BookGenresSection/>
+     <section className="why-us-section">
+          <h2>Why Should You Buy From Us?</h2>
+          <WhyUsSlider />
+        </section>
+        <Footer/>
     </div>
 }
+export default Landing;
