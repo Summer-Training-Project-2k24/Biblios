99162e3f54cdb951c77b8387f9c2c8aaad236202
arpita <arpitaagrahari004@gmail.com>
2024-07-17 00:24:17 +0530

    Re-init of book module

---
+++ Frontend/src/pages/bookpage.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function BookPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Book Page</h1>
+        </div>
+    );
+}
