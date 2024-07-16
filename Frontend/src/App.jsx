cbfc776fb49ed7bd3007923f79d8d7b917d8c308
hiteshkumarroy <hiteshroy011@gmail.com>
2024-07-16 19:50:35 +0530

    l page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ import "./App.css"
 function App() {
 
   return (
-   
+  
 
        <BrowserRouter>
        <Routes>
--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import AuthContext from '../context/AuthContext';
 import './style.css';
-
+import circles from './circles.png';
 
 const AuthForm = () => {
   const { loginUser, signupUser } = useContext(AuthContext);
@@ -32,13 +32,15 @@ const AuthForm = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="circles.png" alt="logo" />
-          Biblios
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div
+              className="w-20 h-20 mr-2"
+              style={{ backgroundImage: `url(${circles})`, backgroundSize: 'cover' }}
+            />
+            Biblios
+       
+        <div className="w-full card rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 md:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-6">
               {isLogin ? 'Sign in to your account' : 'Create a new account'}
