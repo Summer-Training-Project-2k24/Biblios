c474552f546bc6807a3730a6e808739d12b18782
arpita <arpitaagrahari004@gmail.com>
2024-07-17 10:18:56 +0530

    Fixed Book Retrieval

--- Backend/auths/server.js
+++ Backend/auths/server.js
@@ -1,32 +1,21 @@
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
 require('./db'); // Ensure this points to your database connection file
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
-
 const app = express();
-app.use(bodyParser.json());
-app.use(cors()); // Use the cors middleware with your frontend URL
-
-
-
-app.use(bodyParser.json());
-
-
 const userRoutes = require('./routes/userroutes');
-app.use('/user', userRoutes);
-
 const aiRouter = require('./routes/ai');
+
+
+app.use(cors()); // Use the cors middleware with your frontend URL
+app.use(bodyParser.json());
+app.use('/user', userRoutes);
 app.use('/ai', aiRouter);
 
 
 
-// app.listen(3400,()=>{
-//     console.log('listening on port 3400');
-// })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
