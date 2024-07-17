db3e3bff74fe337c06fc7062d18ae73ecfec6202
arpita <arpitaagrahari004@gmail.com>
2024-07-17 15:03:59 +0530

    Updated pages

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                 <ul>
                     <li onClick={() => navigate("/")} className="active">Home</li>
                     <li onClick={() => navigate("/about")}>About Us</li>
-                    <li onClick={() => navigate("/https://66976119b15dac4dff6eeab7--cheerful-frangollo-eac518.netlify.app/")}>Books</li>
+                    <li onClick={() => window.location.href = "https://66976119b15dac4dff6eeab7--cheerful-frangollo-eac518.netlify.app/"}>Books</li>
                     <li onClick={() => navigate("/new-release")}>New Release</li>
                     <li onClick={() => navigate("/contact")}>Contact Us</li>
                     <li onClick={() => navigate("/blog")}>Blog</li>
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
+import a from '../assets/a.jpeg'
+import b from '../assets/b.jpeg'
+import c from '../assets/c.jpeg'
 import './style.css';
 import circles from './circles.png';
 
@@ -98,21 +101,21 @@ const Profile = () => {
             <div className="text-center">
               <span className="flex items-center text-lg font-bold text-gray-900 dark:text-white">
                 Ebooks
-                <img className="ml-2 w-5 h-5" src="https://www.flaticon.com/download/icon/3145385?icon_id=3145385&author=344&team=344&keyword=Download&pack=packs%2Fliterature-33&style=2&format=png&color=%23000000&colored=1&size=512&selection=1&premium=0&type=standard" alt="Ebooks Icon" />
+                <img className="ml-2 w-5 h-5" src={a} alt="Ebooks Icon" />
               </span>
               <p className="text-gray-700 dark:text-gray-300">4(items)</p>
             </div>
             <div className="text-center">
               <span className="flex items-center text-lg font-bold text-gray-900 dark:text-white">
                 Cart
-                <img className="ml-2 w-5 h-5" src="https://www.flaticon.com/download/icon/3514491?icon_id=3514491&author=462&team=462&keyword=Grocery+store&pack=packs%2Fplace-60&style=1223&format=png&color=%23000000&colored=1&size=512&selection=1&premium=0&type=standard&token=03AFcWeA5tY3J-fPwBgjIxfvbLay7CBfV8KbdlgPeLVCpV3LPPL3T5_wXHcDLyayq3CyzmP-FgGGeSkW8yyvsx3g2avkoVQwlv_Khlax7xX-ub8cw-Gcekc2pvbjSmzFuzCzZZ7kpTiEhrhiGW127cI-IsGU4K4u9q2W-u5KiBJrYLOaREMrIu63cLayxA_yeAt9Txb6gcrukxoEB4_O6czrB1EfKlquXZQ7Pub6Stk8abFzZKwlOj7SmIJx9cx9Knp1QeBzN3CI9xK9Tt5sTIpL5izhTX29WDB4LOiiEgBPQ8zidDWhEs_V9US37zp4tDSO3RqqP6qgucbdYY8JYUaal95YIcipxc1Fawzs_1Ce3FNA0ULnR0AoT7c0Z5TMGP9p6z3HBzdCLEWuOPyVp183hegbuPDhknmkuQ4yD3ieZjhZpCTCMhDCfZmhY5Rdrgx2eTZydu9vE0u-hZfiUtsV3OJ4sU6B23CGp_08OIzwWbb_EzFOl5Z1IJOZuCp_rqN_vIIMs89WmoMUyoCGEtp3EhCzmq4Vb7klE4cKhSodaOjOWYnyRyBjRPuX0aCXptGijWlQRoh129AMy2UUhZsc1LIXwgP6ram55SU5uummnkIECvw4ArpBRc16lFjmMnQETET8OXL33SKqqg8cRu4WP_qIvFZQ70gk-M2m_ITRhAiObeVskq1wRCXugOEDjA2eOhKnytgXVL5WKefTHMSTDoc3mWV40TtXRtLAqSi_c3QR8RNJlp-HPFWvpuQ_Q-EDc3KbV36FbyvjU8yvnAC44dlDGqdIhVwt-XiNa9rLILx-p96oU7OWY_oYcRMJqB8slKEL7kLSbKnLWfpy2hHsSJN_AJgfXs3zv9oVN8npzzEJ1jU8gWHncGonrXcaRYMi7eLPeetQ9e70ly5km6rxxUMWFmeZGjcBcGSLAmZ26ZJklKBNsQMXqnEam6Q7lAqW5qba5j6vnyHP7fdielHRB0nlvOa9e3os-9MzHa1HLmvSqWbj9yyXp6JuPdQzZ1O633cSWR--_Jm6tMBkBzOpcFbAQNmGmrprU4YVU3Y4xRT8wqXdWugwMoqGWUSCR6s-FvMQJATXPKydQA4L6V-98rakZLq1u8KFUD4m3ATNI8xYp5oX1UdkVnXokLh8tvzpMkZmzePybEiwuywUEx4l_i2jKpgL4GpCKEJe_3vYi5mGbp-Q065gybWJy5uNWpLf-j8jT1nqzf1VxkGyAxsx0RRPdqoBr86omc1v92BE6MSOoBzYfja1q7RowSIP0-5QcLbMdDayzZ1gp8Y5vzzcNBZM1vFpziOmD0KjNqx9xFTj58FwbPdjttjyjTnSkmjokyrlaHR3NRZgZzYEEo2gYHGN_OPQulQoNNKfpUCup1UL6r0uWWVG4M-OvbF5pLqESbjgzLpECcjZVvGieGz5_6kDWFOhzrripyW9aJ6SrNJMOCN6RuMomEIp7ERM97ABjb-2nDPZdxZT-7g7knzKOHbA_-cabBe_tP9VDiczlBzlh5QMoEjIVNGSjbjnmWi3OPiiQr8UxP-F8kn2m54vTrFvy6E1aJvvqWxcL-GrRnaXUFi7liWYokm3oD-8MOb8j_p0yQjjZ63JkFnOyyCfHJs0u7D6vfJ3qq37GaGwUXqT4mCcGbKi70IQ2xJa6IMF3eCbYNtYKcJYlY-KcBGnfeHVZjki_oEPblvBOl0HXfVkig95zEevNflmRC68LFeDpTLbzgznxkusye-rrzNrTq8ZNeue-oz1qTpZvTC0YxSDuUP-1rMv57dvZ02i6DRW6-qQesxnxQCHjixXuZD6T03y07cOOQvHi3p6IZDnCl7gvWrj15sPKdbv8E711BMVKYDgdsC1l2MQYnCPXKTfK0yOOJrxeNgZ9QDC2j9ciYg0UBMkVSyXYQDhlpy3JhPYHt_1pqEjAmyXs4qkY470Y1fQ_eNdHLGtqLunGL2KVVuhbX6asnRKE" alt="Cart Icon" />
+                <img className="ml-2 w-5 h-5" src={c} alt="Cart Icon" />
               </span>
               <p className="text-gray-700 dark:text-gray-300">12(items)</p>
             </div>
             <div className="text-center">
               <span className="flex items-center text-lg font-bold text-gray-900 dark:text-white">
                 Wishlist
-                <img className="ml-2 w-5 h-5" src="https://www.flaticon.com/download/icon/4379608?icon_id=4379608&author=190&team=190&keyword=Wishlist&pack=packs%2Fsupermarket-150&style=863&format=png&color=%23000000&colored=1&size=512&selection=1&premium=0&type=standard" alt="Wishlist Icon" />
+                <img className="ml-2 w-5 h-5" src={b} alt="Wishlist Icon" />
               </span>
               <p className="text-gray-700 dark:text-gray-300">20(items)</p>
             </div>
