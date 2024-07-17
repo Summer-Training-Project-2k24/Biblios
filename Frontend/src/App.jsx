4f167deb3df4cb630cac9f9a733b69985a170b12
arpita <arpitaagrahari004@gmail.com>
2024-07-17 11:58:10 +0530

    Updated chnages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,11 @@
 import Landing from "./pages/Landing";
 import AboutUs from "./pages/Aboutus";
-<<<<<<< HEAD
-import ContactUs from "./pages/Contactus";
-import Footer1 from "./pages/Footer1";
-=======
 import ContactUs from "./pages/contactus";
 import Footer1 from "./pages/footer1";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
 import Categories from "./components/Categories";
-import Cart from "./components/book/cart";
-import BookPage from "./components/book/bookpage";
-import NotFound from "./components/book/notfound";
 import { useState, useEffect } from 'react';
-import { getCart, removeFromCart, changeQuantity } from './pages/cartFunction'; 
-import { useParams, Link, useHistory } from 'react-router-dom';
-import bookService from './components/book/bookservice';
->>>>>>> 14d1b06164b399399ac0ae76dab5f4a6120430d6
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -39,14 +28,7 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
   
-// >>>>>>> signinsignupfrontend
-=======
-  
->>>>>>> 14d1b06164b399399ac0ae76dab5f4a6120430d6
 
        <BrowserRouter>
         <MoodReader/>
@@ -54,12 +36,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/about" element={<AboutUs/>}/>
           <Route path="/contact" element={<ContactUs/>}/>
-          <Route path="/footer1" element={<Footer1 />}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/book" element={<BookPage/>}/>
-          {/* <Route path="/book/:id" element={<BookPage/>}/> */}
-          <Route path="/notfound" element={<NotFound visible={true} notFoundMessage="Page Not Found" resetLinkText="Back To Homepage" />} />
-                    
+          <Route path="/footer1" element={<Footer1 />}/>   
           </Routes> 
 
           <AuthProvider>
